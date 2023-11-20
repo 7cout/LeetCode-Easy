765cd2ebc34aa74293d4d9ba76e2c41aa345e866
DESKTOP-GBGOJT7\Atian <anton.demagin@yandex.ru>
2023-11-20 18:18:41 +0300

    Solve the first problem - Two Sum
    Added the class Solution.
    Added the method TwoSum of the class Solution.
    Added the simple test.

--- Program.cs
+++ Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace LeetCode_Easy
 {
-    internal class Program
+    internal class Programs
     {
         static void Main(string[] args)
         {
+            Solution solution = new Solution();
+
+            int[] nums = { 3, 3 };
+            int target = 6;
+            int[] answer = { 0, 1 };
+            var result = solution.TwoSum(nums, target);
+            Console.WriteLine("{0}", String.Join("", result));
+            Debug.Assert(answer.SequenceEqual(result));
+
+            nums = new int[2] { 3, 3 };
+            target = 6;
+            answer = new int[2] { 0, 1 };
+            result = solution.TwoSum(nums, target);
+            Console.WriteLine("{0}", String.Join("", result));
+            Debug.Assert(answer.SequenceEqual(result));
+
+            nums = new int[2] { 3, 3 };
+            target = 6;
+            answer = new int[2] { 0, 1 };
+            result = solution.TwoSum(nums, target);
+            Console.WriteLine("{0}", String.Join("", result));
+            Debug.Assert(answer.SequenceEqual(result));
+            Console.WriteLine("Тесты пройдены!");
+
+            Console.WriteLine("Нажмит Enter для завершения работы программы");
+            Console.Read();
         }
     }
 }
---
+++ Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Easy
+{
+    internal class Solution
+    {
+        public int[] TwoSum(int[] nums, int target) 
+        {
+            int indexLeft = 0;
+            int indexRight = 1;
+            int[] indices = new int[2];
+            for (indexLeft = 0; indexLeft <= nums.Length - 2; indexLeft++)
+            {
+                for (indexRight = indexLeft + 1; indexRight <= nums.Length - 1; indexRight++)
+                {
+                    if (nums[indexLeft] + nums[indexRight] == target)
+                    {
+                        indices[0] = indexLeft;
+                        indices[1] = indexRight;
+                        return indices;
+                    }
+                }
+            }
+            return indices;
+        }
+    }
+}
