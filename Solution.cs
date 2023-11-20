bdf173877eb0be9d4b2f95512afc118a932ff61c
DESKTOP-GBGOJT7\Atian <anton.demagin@yandex.ru>
2023-11-20 18:41:48 +0300

    Optimize the solution
    Optimized the solution Two Sum with dictionary.

--- Solution.cs
+++ Solution.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,24 +9,19 @@ namespace LeetCode_Easy
 {
     internal class Solution
     {
-        public int[] TwoSum(int[] nums, int target) 
+        public int[] TwoSum(int[] nums, int target)
         {
-            int indexLeft = 0;
-            int indexRight = 1;
-            int[] indices = new int[2];
-            for (indexLeft = 0; indexLeft <= nums.Length - 2; indexLeft++)
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
             {
-                for (indexRight = indexLeft + 1; indexRight <= nums.Length - 1; indexRight++)
+                int a = target - nums[i];
+                if (dic.ContainsKey(a))
                 {
-                    if (nums[indexLeft] + nums[indexRight] == target)
-                    {
-                        indices[0] = indexLeft;
-                        indices[1] = indexRight;
-                        return indices;
-                    }
+                    return new int[] { dic[a], i };
                 }
+                dic[nums[i]] = i;
             }
-            return indices;
+            return new int[] { };
         }
     }
 }
